060ab2521e11626c7349ad7bf8b930606db0803f
JOAO PEDRO SILVEIRA DE LIMA <joao.lima1@aluno.senac.rio>
2026-07-17 20:44:53 -0300

    Atualização 17/07/2026

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth/auth-interceptor';
+
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),provideHttpClient()
+    provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { EpiList } from './epi/epi-list/epi-list';
 import { EpiDetalhe } from './epi/epi-detalhe/epi-detalhe';
 import { EpiForm } from './epi/epi-form/epi-form';
+import { Login } from './auth/login/login';
+import { Unauthorized } from './auth/unauthorized/unauthorized';
+import { authGuard } from './auth/auth-guard';
+
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'epi', pathMatch: 'full' },
     { path: 'epi', component: EpiList },
     { path: 'epi/detalhe', component: EpiDetalhe },
-    { path: 'epi/cadastro', component: EpiForm }
+    { path: 'epi/cadastro', component: EpiForm },
+    { path: 'login', component: Login },
+    { path: 'unauthorized', component: Unauthorized },
+    { path: 'epi', component: EpiList, canActivate: [authGuard] }
 ];
